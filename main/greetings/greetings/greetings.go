3fc3a63e01e6fdcdf2f39f7b210e3d86cced6ecd
Hiroto.Ito <hiroto.ito@systemi.co.jp>
2024-07-30 23:54:34 +0900

    add: error handling for empty name in Hello function

--- main/greetings/greetings/greetings.go
+++ main/greetings/greetings/greetings.go
@@ -1,9 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// 名前が定義されていない場合は、メッセージと共にエラーを返却する
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// := 宣言と初期化を行うショートカット
 	message := fmt.Sprintf("Hi, %v. welcome!", name)
-	return message
+	return message, nil
 }
--- main/greetings/hello/hello.go
+++ main/greetings/hello/hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	// 他のフォルダのファイルのimport方法
 	"greetings/greetings"
 )
 
 func main() {
+
+	// Loggerのプロパティを設定
+	log.SetPrefix("greetings: ")
+	// ログエントリのプリフィックスと印刷を無効にするフラグ
+	log.SetFlags(0)
+
 	// greetingsファイルにて作成したHello関数を呼び出す
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+
+	// エラーが返却された場合はコンソールにエラーを出力し、プログラムを終了する。
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
